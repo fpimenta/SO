7e89a4e45f83211ae692ef917b5bc4907657f7be
PeachBug <33940272+PeachBug@users.noreply.github.com>
2018-02-24 17:29:15 +0000

    Update ex1b.c

--- Aula 2/ex1b.c
+++ Aula 2/ex1b.c
@@ -1,28 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <errno.h>
 
 #define BUF_SIZE 64
 
 int main(int argc,char* argv[]){
-	int filedes, rbytes, totby;
+	int sourcef, newf, rbytes, totby;
 	char buff[BUF_SIZE];
 	totby = 0;
 	
-	if(argc!=2){
-		printf("Error: Argument is missing!");
+	if(argc!=3){
+		printf("Error: Arguments are missing!\nArguments: [SOURCE_FILE] [NEWFILE]\n");
 		exit(1);
 	}
-	filedes = open(argv[1],O_RDONLY);
+	
+	sourcef = open(argv[1],O_RDONLY);
+	newf = open(argv[2],O_WRONLY | O_CREAT | O_EXCL, 0644);
+	if(newf == -1 && errno == EEXIST){
+		printf("Error: %s already exists!\n", argv[2]);
+		exit(1);
+	}
 	
 	while(1){
-		rbytes = read(filedes, buff, BUF_SIZE);
-		write(STDOUT_FILENO, buff, rbytes);
+		rbytes = read(sourcef, buff, BUF_SIZE);
+		write(newf, buff, rbytes);
 		totby = totby + rbytes;
 		if(rbytes <= 0) break;
 	}
-	printf("\n");
-	close(filedes);
+	printf("The file was successfully copied to %s.\n",argv[2]);
+	close(sourcef);
+	close(newf);
 }
