805d4af941590ae89f6852c9d863ce1e92870909
fpimenta <franciscofernandespimenta@gmail.com>
2018-04-10 19:26:13 +0100

    Create ppd.c

---
+++ Testes e Exames/PPs/pp2016abr/Solucoes/ppd.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <fcntl.h>
+
+#define max 500
+int match_line(int fd, char *str);
+void *pesquisa(void *arg);
+char *str;
+typedef struct {
+    int fd;
+    char *name;
+} pesquisa_arg;
+
+pthread_mutex_t mut = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t c = PTHREAD_COND_INITIALIZER;
+
+int done=0;
+
+    
+int main(int argc, char *argv[]){
+    int i;
+    int N = argc-2;
+    str = argv[1];
+    pthread_t tid[N];
+    pesquisa_arg t_arg[N];
+    
+    for (i=0; i<(N); i++){
+        t_arg[i].fd = open(argv[i+2], O_RDONLY);
+        t_arg[i].name = argv[i+2];
+        
+    }
+    
+    for (i=0; i<(N); i++){
+       pthread_create(&tid[i], NULL, pesquisa, &t_arg[i]);
+    }
+    
+    pthread_mutex_lock(&mut);
+    while(done!=3){
+        pthread_cond_wait(&c, &mut);
+    }
+    pthread_mutex_unlock(&mut);
+    
+    
+}
+
+void *pesquisa(void *arg){
+    pesquisa_arg t_arg = *(pesquisa_arg *)arg;
+    int line[max], i=0, j=0;
+     while(1){
+         line[i] = match_line(t_arg.fd, str);
+         if(line[i]==0)     
+             break;
+         i++;
+    }
+    
+    pthread_mutex_lock(&mut);
+    for(j=0; j<i; j++){
+         printf("%s:\t%d\n", t_arg.name, line[j]);
+    }
+    pthread_mutex_unlock(&mut);
+    pthread_mutex_lock(&mut);
+    done++;
+    pthread_cond_signal(&c);
+    pthread_mutex_unlock(&mut);
+    return NULL;
+        
+}
