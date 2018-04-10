f03ce1f08b6e340b291c86c5a79056346c4fda40
fpimenta <franciscofernandespimenta@gmail.com>
2018-04-10 15:28:46 +0100

    Create ppd.c
    Solution to the last exercise of test May 20th 2016

---
+++ Testes e Exames/Exames/Solucoes/1516_20maio/ppd.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <string.h>
+
+long long du(int fd);
+
+pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+pthread_mutex_t mut = PTHREAD_MUTEX_INITIALIZER;
+int done = 0;
+
+typedef struct X{
+    char *file;
+    long long int size;
+} X;    
+
+void *thrfunc(void * arg){
+    X *dat = arg;
+    int f;
+    
+    
+    if( (f = open(dat->file,O_RDONLY)) != -1 ){
+        dat->size = du(f);
+    }
+    
+    
+    pthread_mutex_lock(&mut);
+    done++;
+    pthread_cond_signal(&cond);
+    pthread_mutex_unlock(&mut);
+    pthread_exit(NULL);
+} 
+
+int main(){
+    
+   
+    X * data;
+    data = (X *) malloc(sizeof(X)*10);
+    char buf[255];
+    register int nfiles = 0;
+    
+   
+    while(scanf(" %s", buf ) == 1){
+            data[nfiles].file = (char*)malloc(sizeof(char*)*255);
+            strcpy(data[nfiles].file, buf);
+            nfiles++;
+    }   
+    pthread_t idT[nfiles];
+    for(int i = 0; i < nfiles;i++){
+      pthread_create(&idT[i],NULL,thrfunc,&data[i]);  
+    }   
+    
+    pthread_mutex_lock(&mut);
+    while( done < nfiles){
+     pthread_cond_wait(&cond,&mut);
+    }
+    pthread_mutex_unlock(&mut);
+    for(int i = 0; i < nfiles; i++){
+        printf("%s:\t%lld bytes\n",data[i].file,data[i].size);
+    }    
+    
+    exit(EXIT_SUCCESS);
+}    
